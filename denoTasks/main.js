bbee937cccc01eebf5e1346968bb3ca4b0ec3e57
Paolo Di Pasquale <paolo@Paolos-MacBook-Pro.local>
2020-11-02 16:43:29 +0000

    Get components list

--- denoTasks/main.js
+++ denoTasks/main.js
@@ -1,23 +1,15 @@
-// get content data
-// get all files in views dir
-// read view file
-// get components
-// get component attributes
-// pass data to components
-// render components
-// write file
-
 const decoder = new TextDecoder();
 const postContentDir = './content/posts/';
+const viewsDir = './src/templates/views/';
+
+// Get posts content and store in JSON file
 let posts = [];
 
 for await (const dirEntry of Deno.readDir(postContentDir)) {
-	console.log(dirEntry.name);
-
 	const data = await Deno.readFile(postContentDir + dirEntry.name);
 	const content = decoder.decode(data);
-	const postDateString = content.match(/data-published-date="(.+)"/)[1];
-	const [year, month, day, h, m, s] = postDateString.split('-');
+	const dateString = content.match(/data-published-date="(.+)"/)[1];
+	const [year, month, day, h, m, s] = dateString.split('-');
 	const timestamp = new Date(year, month -1, day, h, m, s).getTime(); // month is zero indexed
 	const author = content.match(/data-author="(.+)"/)[1];
 	const title = content.match(/<h2>(.+)<\/h2>/)[1];
@@ -25,6 +17,15 @@ for await (const dirEntry of Deno.readDir(postContentDir)) {
 	posts.push({timestamp, author, title, content});
 }
 
-console.log(posts);
-
 Deno.writeTextFile('./posts.json', JSON.stringify(posts));
+
+// Render views
+for await (const dirEntry of Deno.readDir(viewsDir)) {
+	console.log(dirEntry.name);
+
+	const data = await Deno.readFile(viewsDir + dirEntry.name);
+	const content = decoder.decode(data);
+	const componentList = [...content.matchAll(/<itanglo-(.+).*><\/itanglo-\1>/g)].map(item => item[1]);
+	
+	console.log(componentList);
+}
