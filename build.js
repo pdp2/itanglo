cf6e65a0fd385f2d61820952161db6b8694ac8fc
Paolo Di Pasquale <pdp2.paolo@gmail.com>
2021-02-24 16:10:07 +0000

    wip

--- build.js
+++ build.js
@@ -2,6 +2,8 @@
 * This code should be executed using Deno.
 * The command to do this is deno run --allow-read --allow-write build.js
 */
+console.log('Starting build task. \n');
+
 const decoder = new TextDecoder();
 // read index file
 const indexFile = await Deno.readFile('./src/index.html');
@@ -17,10 +19,12 @@ includeTagMatches.forEach(async (match, index, array) => {
 	const includeFile = await Deno.readFile(src);
 	const includeContent = decoder.decode(includeFile);
 	// replace include tag with file
-	indexContent.replace(includeTag, includeContent);
+	indexContent = indexContent.replace(includeTag, includeContent);
 	// on last iteration write index file in docs folder
 	if (!array[index + 1]) {
-		Deno.writeFile('./docs/index.html', indexContent);
+		const indexTargetPath = './docs/index.html';
+		Deno.writeTextFile(indexTargetPath, indexContent);
+		console.log(`Created ${indexTargetPath}. \n`);
 	}
 });
 
